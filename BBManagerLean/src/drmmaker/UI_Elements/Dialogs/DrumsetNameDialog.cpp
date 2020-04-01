35fb2f05c5b40805a84de3903bbd95723c392300
JulissaDantes <julissa@singularsound.com>
2020-04-01 16:28:45 -0400

    Closes #4

--- BBManagerLean/src/drmmaker/UI_Elements/Dialogs/DrumsetNameDialog.cpp
+++ BBManagerLean/src/drmmaker/UI_Elements/Dialogs/DrumsetNameDialog.cpp
@@ -1,3 +1,17 @@
+/*
+  	This software and the content provided for use with it is Copyright © 2014-2020 Singular Sound 
+ 	BeatBuddy Manager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as published by
+    the Free Software Foundation.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include "DrumsetNameDialog.h"
 #include "../../Model/drmmakermodel.h"
 #include "math.h"
