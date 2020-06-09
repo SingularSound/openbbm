e791b1fa4af8dc07c5261cc7d12d57193178aebe
JulissaDantes <julissa@singularsound.com>
2020-06-09 08:41:00 -0400

    Restoring singletrack play

--- BBManagerLean/src/player/songPlayer.cpp
+++ BBManagerLean/src/player/songPlayer.cpp
@@ -701,12 +701,13 @@ void SongPlayer_SetSingleTrack(MIDIPARSER_MidiTrack *track) {
 
 void SongPlayer_ProcessSingleTrack(float ratio, int32_t nTick, int32_t offset) {
     TmpMasterPartTick = MasterTick + nTick;
-    int pickuplength = 0-SingleMidiTrackPtr->pickupNotesLength;//pick up notes in a variable so the code is easier to understand
+    int pickuplength = 0;//pick up notes
     if (PlayerStatus == NO_SONG_LOADED) {
         return;
     }
 
     if (PlayerStatus == SINGLE_TRACK_PLAYER) {
+        pickuplength = (SingleMidiTrackPtr->event[0].tick < 0 && MasterTick == 0)? SingleMidiTrackPtr->event[0].tick: pickuplength;
 
         TrackPlay(SingleMidiTrackPtr,MasterTick - offset + pickuplength ,TmpMasterPartTick - offset + pickuplength,ratio,0,MAIN_PART_ID);
 
